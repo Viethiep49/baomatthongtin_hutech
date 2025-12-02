efe62979bf9f4a713e6808b16d9e7b2fa210a504
Viethiep49 <truongviethiep49@gmail.com>
2025-12-02 19:11:03 +0700

    tuan1

---
+++ TVHiep_0637_tuan1/TVHiep_0637_tuan1/src/main/java/com/mycompany/tvhiep_0637_tuan1/TVHiep_0637_tuan1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tvhiep_0637_tuan1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TVHiep_0637_tuan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
